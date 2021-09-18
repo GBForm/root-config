db08251c4805a356c442cae7d5c8838f116e52d7
Sam Carbine <scarbine@me.com>
2021-09-18 17:39:22 -0500

    Fixed errors in the root-config file where commas were missing after dependencies

--- src/built-root-config.ts
+++ src/built-root-config.ts
@@ -1,24 +1,24 @@
 import { registerApplication, start } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@built/loan-application",
+  app: () => System.import("@built/loan-application"),
   activeWhen: ["/"],
 });
-
-// registerApplication({
-//   name: "@built/loan-application",
-//   app: () => System.import("@built/loan-application"),
-//   activeWhen: ["/"]
-// });
-// registerApplication({
-//   name: "@built/success",
-//   app: () => System.import("@built/success"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+  name: "@built/success",
+  app: () => System.import("@built/success"),
+  activeWhen: ["/"],
+});
 
 start({
   urlRerouteOnly: true,
